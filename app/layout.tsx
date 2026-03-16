51bea24b2cb9f30c90e387e1ae7834409c12a9a8
Khalil Ltaief <ltaief.khalil@gmail.com>
2026-03-16 12:27:32 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-amber-50 antialiased`}
       >
         {children}
       </body>
